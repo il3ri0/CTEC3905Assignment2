cddd36b6127d3e750e0538abe770eeca36a23c8e
Ileri Onawoga <Ileri@ileris-air.home>
2017-12-28 22:12:48 +0000

    almost finshed product

--- resume.js
+++ resume.js
@@ -6,15 +6,15 @@ $(function () {
 				return false;
 			}
 
-			var link = $(this).children().attr('href');//ссылка на текст для показа
-			var prevActive = $('li.active').children().attr('href'); // ссылка на текст пока что активной вкладки
-			//console.log(prevActive);
+			var link = $(this).children().attr('href');
+			var prevActive = $('li.active').children().attr('href');
+
 			$('li.active').removeClass('active');
 			$(this).addClass('active');
-			//скрываем.показываем текст
+
 			$(prevActive).fadeOut(500, function () {
 				$(link).fadeIn();
-				//изменяем цвет
+
 				$('#items li').css('background', '#ccc');
 				$('li.active').css('background', '#f3f3f3');
 			});
